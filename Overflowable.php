3e5a0499c2fc28bb3c61b27db29eba0b7a526b8a
Daniel <32772927+DanielGilB@users.noreply.github.com>
2020-01-13 14:35:34 +0100

    refactor duplicated code into new method

---
+++ Overflowable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CraftLogan\LaravelOverflow;
+use Illuminate\Support\Facades\Schema;
+
+trait Overflowable{
+
+    public function allWithOverflow()
+    {
+        $properties[$this->overflow_column] = $this->overflow();
+        return array_merge($properties, $this->getColumns());
+    }
+
+    public function getColumns()
+    {
+        $columnNames = $this->getColumnNames();
+        $attributes = array_intersect_key($this->all(), $columnNames);
+        return $attributes;
+    }
+
+    public function overflow()
+    {
+        $columnNames = $this->getColumnNames();
+        $attributes = array_diff_key($this->all(), $columnNames);
+        $attributes = json_encode($attributes);
+        return $attributes;
+    }
+
+    public function getTableColumns()
+    {
+        return Schema::getColumnListing($this->table);
+    }
+
+    public function getColumnNames()
+    {
+        $columnNames = $this->getTableColumns();
+        $columnNames = array_fill_keys($columnNames, "");
+        return $columnNames;
+    }
+}
